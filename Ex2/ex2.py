128ccfb20bc62ed1a3daa8783ed7c3f976e8a1fd
amirhossein <ahpd1068@yahoo.com>
2018-11-09 15:04:56 +0100

    completing ex2

--- Ex2/ex2.py
+++ Ex2/ex2.py
@@ -1,15 +1,24 @@
 import numpy as np
+import time
 
-def calcPrime():
-    primes = []
-    for i in range(2, 100):
+def calcPrime(n):
+    t = time.time()
+    primes = [2,3]
+    for i in range(2, n):
         isPrime = True
-        for j in range(2,i):
+        for j in primes:
             if (i % j == 0):
                 isPrime = False
         if(isPrime):
             primes.append(i)
-    return primes
+    return primes , (time.time() - t)
 
 if __name__ == '__main__':
-    calcPrime()
+    prList1 = []
+    tim1 = 0.0
+    prList1 , tim1 = calcPrime(1000)
+    prList2 = []
+    tim2 = 0.0
+    prList2 , tim2 = calcPrime(10000)
+    print('Elapsed time for calculating prime numbers 0 - 1000:', tim1)
+    print('Elapsed time for calculating prime numbers 0 - 10000:', tim2)
--- Ex2/test_ex2.py
+++ Ex2/test_ex2.py
@@ -4,7 +4,7 @@ import ex2
 class testCase(unittest.TestCase):
 
     def test_calcPrime(self):
-        self.assertEqual(ex2.calcPrime(), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+        self.assertEqual(ex2.calcPrime(100)[0], [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
 
 if __name__ == '__main__':
     unittest.main()
