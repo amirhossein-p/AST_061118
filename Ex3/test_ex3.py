0e76b14e125abce9dbd390b096d0fb844c26708e
Sidra <sidra.rashid@smail.inf.h-brs.de>
2018-11-09 16:24:04 +0100

    changes in ex4

--- Ex3/test_ex3.py
+++ Ex3/test_ex3.py
@@ -1,11 +1,28 @@
 import unittest
+import numpy as np
 import ex3
 
 class testCase(unittest.TestCase):
-
+    """Tests for `isTest.py`."""
     def test_fn1(self):
-        self.assertEqual(ex3.fn1(5), [0,2,4,6,8])
+        self.assertEqual(ex3.fn1(2),[0,2,4])
 
+    def test_fn2(self):
+        self.assertEqual(ex3.fn2(2),[0,1,1.41421356237])
+    
+
+    def test_fn3(self):
+        self.assertEqual(ex3.fn3(2),[1,10,100])
+
+    def test_fn4(self):
+        self.assertEqual(ex3.fn4(2),[0,1,8])
+
+    def test_fn5(self):
+        self.assertEqual(ex3.fn5(2),[0,1,8])
+
+
+    def test_fn4(self):
+        self.assertEqual(ex3.fn4(2),[1,10,100])
 
 if __name__ == '__main__':
     unittest.main()
--- Ex4/ex4.py
+++ Ex4/ex4.py
@@ -1,21 +1,20 @@
 import math
 import numpy as np
-print(math.floor(np.pi))
+
 class Circle:
     
     
-    def __init__(self,p,d):
-        if p <= 0:
-            p = 2
-        self.precision = p
+    def __init__(self,d):
         self.diameter = d
         self.radius = d/2
 
 
-    def CalcCircumference(self):
+    def CalcCircumference(self,p):
+        if p <= 0:
+            p = 2
         Circumferences = []
         Circ_percent_changes = []
-        for i in range(self.precision+1):
+        for i in range(p+1):
             pi = math.floor(np.pi*10**i)/10**i
             C = pi*self.diameter
             Circumferences.append(C)
@@ -28,10 +27,12 @@ class Circle:
             print('\n')
         return Circumferences, Circ_percent_changes
      
-    def CalcArea(self):
+    def CalcArea(self,p):
+        if p <= 0:
+            p = 2
         Areas = []
         Area_percent_changes = [] 
-        for i in range(self.precision+1):
+        for i in range(p+1):
             pi = math.floor(np.pi*10**i)/10**i
             A = pi*(self.radius**2)
             Areas.append(A)
@@ -39,16 +40,16 @@ class Circle:
             print("The value of pi is ", pi)
             print("The Area of this circle is: ", Areas[i])
             if len(Areas) > 1:
-                Area_percent_changes.append(round((abs(Areas[i-1]-Areas[i])/Areas[i])*100),3)
-                print("% change in Area: ", round((abs(Areas[i-1]-Areas[i])/Areas[i])*100),3)
+                Area_percent_changes.append(round((abs(Areas[i-1]-Areas[i])/Areas[i])*100,3))
+                print("% change in Area: ", round((abs(Areas[i-1]-Areas[i])/Areas[i])*100,3))
             print('\n')
 
         return Areas, Area_percent_changes
 
-diameter = input("Enter the diameter of a circle ")
-precision = input("Enter the precision value for pi ")
+diameter = int(input("Enter the diameter of a circle "))
+precision = int(input("Enter the precision value for pi "))
 
 
-C = Circle(precision,diameter)
-Areas,percent_change_area = C.CalcArea()
-Circumferences, percent_change_circumferences = C.CalcCircumference()
+C = Circle(diameter)
+Areas,percent_change_area = C.CalcArea(precision)
+Circumferences, percent_change_circumferences = C.CalcCircumference(precision)
